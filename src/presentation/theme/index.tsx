1635fe49b1f64b361c6d64ff893846787cc59d63
Rozane Souza <rozane.souza.silva@hotmail.com>
2023-04-15 18:52:58 -0300

    feat: add theme config

---
+++ src/presentation/theme/index.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import {useColorScheme} from 'react-native';
+import {ThemeProvider} from 'styled-components';
+
+import DarkTheme from './dark';
+import LightTheme from './light';
+import {ThemeColors, ThemeProps, ThemeType} from './types';
+
+export const themes = {
+  dark: DarkTheme,
+  light: LightTheme,
+};
+
+const Context = React.createContext<Required<ThemeType>>(LightTheme);
+
+export const getThemeColor =
+  (color: keyof ThemeColors) =>
+  ({theme}: {theme: ThemeType}) =>
+    theme.colors[color];
+
+export const useTheme = (): Required<ThemeType> => React.useContext(Context);
+
+export const useThemeCurrent = () => {
+  const deviceColorSchema = useColorScheme();
+  const theme = deviceColorSchema ? themes[deviceColorSchema] : themes.light;
+
+  return {
+    ...theme,
+    getThemeColor,
+  };
+};
+
+export const Theme: React.FC<React.PropsWithChildren<ThemeProps>> = ({
+  children,
+  theme,
+}) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Context.Provider value={theme}>{children}</Context.Provider>
+    </ThemeProvider>
+  );
+};
---
+++ src/presentation/theme/light.ts
@@ -0,0 +1,18 @@
+import {ThemeType} from './types';
+
+const LightTheme: ThemeType = {
+  colors: {
+    text: '#65666a',
+    light: '#abacb3',
+    header: '#16171e',
+    black: '#000000',
+    white: '#ffffff',
+    shape: '#fafafbff',
+    primary: '#556fffff',
+    background: '#ffffff',
+    backgroundEnd: '#fafafa',
+    statusBar: 'light-content',
+  },
+};
+
+export default LightTheme;
