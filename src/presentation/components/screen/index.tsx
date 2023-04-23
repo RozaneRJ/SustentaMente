3bb4504c8a40a45531ec69befa097828770fa2ed
Rozane Souza <rozane.souza.silva@hotmail.com>
2023-04-23 13:36:55 -0300

    refactor: add scroll visibility option

--- src/presentation/components/screen/index.tsx
+++ src/presentation/components/screen/index.tsx
@@ -7,19 +7,33 @@ import {FullScreen, ContentScreen} from './styles';
 export type ScreenProps = {
   isScrollableContent?: boolean;
   color?: keyof ThemeColors;
+  showsVerticalScrollIndicator?: boolean;
+  showsHorizontalScrollIndicator?: boolean;
 };
 
 export const Screen: React.FC<React.PropsWithChildren<ScreenProps>> =
-  React.memo(({isScrollableContent = true, color, children}) => {
-    const content = !isScrollableContent ? (
-      children
-    ) : (
-      <ScrollView>{children}</ScrollView>
-    );
+  React.memo(
+    ({
+      isScrollableContent = true,
+      color,
+      children,
+      showsVerticalScrollIndicator,
+      showsHorizontalScrollIndicator,
+    }) => {
+      const content = !isScrollableContent ? (
+        children
+      ) : (
+        <ScrollView
+          showsVerticalScrollIndicator={showsVerticalScrollIndicator}
+          showsHorizontalScrollIndicator={showsHorizontalScrollIndicator}>
+          {children}
+        </ScrollView>
+      );
 
-    return (
-      <FullScreen color={color}>
-        {children && <ContentScreen color={color}>{content}</ContentScreen>}
-      </FullScreen>
-    );
-  });
+      return (
+        <FullScreen color={color}>
+          {children && <ContentScreen color={color}>{content}</ContentScreen>}
+        </FullScreen>
+      );
+    },
+  );
