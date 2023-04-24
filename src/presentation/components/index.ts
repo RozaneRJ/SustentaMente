47944a1e35ea6bd85bea9c108b5bbeddfb71a371
Rozane Souza <rozane.souza.silva@hotmail.com>
2023-04-23 22:11:31 -0300

    feat: add header component

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -2,5 +2,6 @@ export * from './card';
 export * from './badge';
 export * from './avatar';
 export * from './screen';
+export * from './header';
 export * from './card-simple';
 export * from './header-avatar';
